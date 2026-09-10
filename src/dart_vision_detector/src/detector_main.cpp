65798d801f56a291f5ae196e3b55684b5e44a052
zfxlab <zfxcode@gmail.com>
2026-09-10 23:25:26 +0800

    fix!: delete the useless codes

--- src/dart_vision_detector/src/detector_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "dart_vision_detector/detector_node.hpp"
-
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<dart_vision::detector::DetectorNode>(rclcpp::NodeOptions{}));
-    rclcpp::shutdown();
-    return 0;
-}
--- src/dart_vision_serial/src/serial_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "dart_vision_serial/serial_node.hpp"
-
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<dart_vision::serial::SerialNode>(rclcpp::NodeOptions{}));
-    rclcpp::shutdown();
-    return 0;
-}
