e0293b09f0967252eaf84b93846d13fd5dd452f7
zfxlab <zfxcode@gmail.com>
2026-09-10 22:55:58 +0800

    feat(target_estimation): add empty dart_target_estimation

---
+++ src/dart_target_estimation/include/dart_target_estimation/kalman_filter.hpp
@@ -0,0 +1,8 @@
+#ifndef DART_TARGET_ESTIMATION_KALMAN_FILTER_HPP
+#define DART_TARGET_ESTIMATION_KALMAN_FILTER_HPP
+
+namespace dart_vision::target_estimation {
+    
+} // namespace dart_vision::target_estimation
+
+#endif // DART_TARGET_ESTIMATION_KALMAN_FILTER_HPP
---
+++ src/dart_target_estimation/src/kalman_filter.cpp
@@ -0,0 +1,10 @@
+#include "dart_target_estimation/kalman_filter.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <numbers>
+#include <stdexcept>
+
+namespace dart_vision::target_estimation {
+
+} // namespace dart_vision::target_estimation
